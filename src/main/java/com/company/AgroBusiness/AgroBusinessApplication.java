2c341e3f940926b6de06aecc0a41506dd65772dd
Jesse Ndamutsa <jndamutsa@mum.edu>
2020-02-02 23:11:18 -0600

    Initial commit

---
+++ src/main/java/com/company/AgroBusiness/AgroBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.company.AgroBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroBusinessApplication.class, args);
+	}
+
+}
